271d284a0bb4569e9a46dfe283e534e2498401e9
Keming <kemingy94@gmail.com>
2026-06-22 23:37:23 +0800

    feat: bump to rand_core 0.10.1, edition 2024, drop py39
    Signed-off-by: Keming <kemingy94@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //! ## Examples
 //!
 //! ```
-//! use rand_core::{RngCore, SeedableRng};
+//! use rand_core::{Rng, SeedableRng};
 //! use rule30::ExtendedCA;
 //!
 //! let mut ca = ExtendedCA::seed_from_u64(42);
@@ -20,7 +20,7 @@ pub mod prelude;
 mod rule30;
 
 // Re-exports from rand_core
-pub use rand_core::{RngCore, SeedableRng};
+pub use rand_core::{Rng, SeedableRng};
 
 pub use self::extended_ca::ExtendedCA;
 pub use self::rule30::Rule30;
